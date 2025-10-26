0d226dc75398991aa71a7dd7c4a2a244223ab095
glozajoza <glozajoza@gmail.com>
2025-10-26 13:51:24 +0100

    Small fix

--- Skripta.sh
+++ Skripta.sh
@@ -12,9 +12,24 @@ URL2="http://localhost:5601"
 
 # ==== FUNCTIONS ====
 
+docker_preconfig() {
+	echo "[INFO] Docker prerequiements"
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+	
+}
+
 install_packages() {
     echo "[INFO] Installing required packages..."
-    sudo apt install -y keepalived docker docker-compose
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin keepalived
 }
 
 
